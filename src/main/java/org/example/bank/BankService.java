6a0c530dac1a48951a90c8cc987a840bee868775
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-02 16:12:21 +0200

    Create BankService class and add transferMoney method

---
+++ src/main/java/org/example/bank/BankService.java
@@ -0,0 +1,28 @@
+package org.example.bank;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankService {
+
+    public String getCustomerNumberOfNewCustomer(Client client) {
+        Account account = new Account("d1234", new BigDecimal("10000.00"), client);
+        return account.getAccountNumber();
+    }
+
+    public void transferMoney(ArrayList<Account> accounts, String accountNumberFrom, String accountNumberTo, BigDecimal amount) {
+        BigDecimal accountBalanceFrom;
+        BigDecimal accountBalanceTo;
+
+        for(Account account: accounts){
+            if(account.getAccountNumber().equals(accountNumberFrom)) {
+                account.setAccountBalance(account.getAccountBalance().subtract(amount));
+            } else if(account.getAccountNumber().equals(accountNumberTo)) {
+                account.setAccountBalance(account.getAccountBalance().add(amount));
+            }
+        }
+        System.out.println("Money has been sent");
+    }
+
+}
